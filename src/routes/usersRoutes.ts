2eaea459e0a105910885b6406955f9dc4844e3ec
João Vitor Aguiar <121316279+JoaoVitorAguiar@users.noreply.github.com>
2023-12-15 10:09:25 -0300

    feat: add user routes

---
+++ src/routes/usersRoutes.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { UsersController } from "../controllers/UsersController";
+import { ensureAuthenticate } from "../middlewares/ensureAuthenticate";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+
+const usersRoutes = Router();
+const controller = new UsersController();
+
+// Usuário não precisa esta autenticado
+usersRoutes.post('/create', controller.create) 
+
+usersRoutes.use(ensureAuthenticate);
+
+usersRoutes.get('/show/:id', controller.show) 
+usersRoutes.put('/update/:id', controller.update)
+usersRoutes.delete('/delete/:id', controller.delete)  
+
+usersRoutes.use(ensureAdmin);
+// Usuário precisa ser admin
+usersRoutes.get('/list', controller.list) 
+
+export {usersRoutes}
