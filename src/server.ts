a6f992b52db4ad0853aced78833d72cfcedb66bb
João Vitor Aguiar <121316279+JoaoVitorAguiar@users.noreply.github.com>
2023-12-20 08:53:47 -0300

    feat: add error Interceptor

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ app.use(cors());
 
 app.use(express.json());
 app.use(routes);
-// app.use(errorInterceptor);
+app.use(errorInterceptor);
 
 app.listen(3333, () => {
     console.log("Server started on port 3333")
